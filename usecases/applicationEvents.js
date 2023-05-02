e38d3209491fe3a14e9ae067a0b12ecca526d58e
Brendan Campbell-Hartzell <50385408+b-camphart@users.noreply.github.com>
2023-05-01 23:18:38 -0700

    Rename "UserActivity" type to "PlayerActivity"

--- usecases/applicationEvents.js
+++ usecases/applicationEvents.js
@@ -3,7 +3,7 @@ exports.PlayerActivityChanged = class PlayerActivityChanged {
      *
      * @param {string} gameId - The id of the game for which the player activity changed
      * @param {string} playerId - The id of the player for which the activity changed
-     * @param {import("../entities/Game").UserActivity} activity - The new activity for the player in the game
+     * @param {import("../entities/Game").PlayerActivity} activity - The new activity for the player in the game
      */
     constructor(gameId, playerId, activity) {
         this.gameId = gameId;
